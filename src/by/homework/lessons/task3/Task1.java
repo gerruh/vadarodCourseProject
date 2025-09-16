f73b4628553257913ca4c25395b6a7cd41d539fc
Herman Volzhsky <avvewyccstrila@gmail.com>
2025-09-16 23:10:29 +0300

    Task 3 HW

---
+++ src/by/homework/lessons/task3/Task1.java
@@ -0,0 +1,15 @@
+package by.homework.lessons.task3;
+
+/*
+Дано уравнение x/6+25=98. Найти x и вывести его в консоль
+ */
+
+public class Task1 {
+    public static void main(String[] args) {}
+
+    public static void run() {
+        // x/6 + 25 = 98
+
+        System.out.println((98-25)*6);
+    }
+}
